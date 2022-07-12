b2ebe2572abb8dba5d6380ab49f532b1db608355
Anastasiia Rakhmatova <rakhmatova-93@mail.ru>
2022-07-12 16:34:23 +0300

    Create MainScreen and PlaceCard components

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 
+const Settings = {
+  CARDS_ON_PAGE: 5
+};
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
 root.render(
   <React.StrictMode>
-    <App />
+    <App cardsOnPage={Settings.CARDS_ON_PAGE}/>
   </React.StrictMode>,
 );
