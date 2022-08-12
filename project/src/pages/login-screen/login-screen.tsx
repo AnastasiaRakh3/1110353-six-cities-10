4727ec408135ff4d573aba26fac58b88c9b8c98b
Anastasiia Rakhmatova <rakhmatova-93@mail.ru>
2022-08-12 21:01:11 +0300

    Implement authorization on login screen

--- project/src/pages/login-screen/login-screen.tsx
+++ project/src/pages/login-screen/login-screen.tsx
@@ -1,6 +1,32 @@
+import { useState, ChangeEvent, FormEvent } from 'react';
+
 import Logo from '../../components/logo/logo';
+import { useAppDispatch } from '../../hooks/index';
+import { loginAction } from '../../store/api-actions';
+import { AuthData } from '../../types/auth-data';
 
 export default function LoginScreen(): JSX.Element {
+
+  const dispatch = useAppDispatch();
+
+  const [authData, setAuthData] = useState<AuthData>({ login: '', password: '' });
+
+  const handleLoginChange = (evt: ChangeEvent<HTMLInputElement>): void => {
+    evt.preventDefault();
+    setAuthData({ ...authData, login: evt.target.value });
+  };
+
+  const handlePasswordChange = (evt: ChangeEvent<HTMLInputElement>): void => {
+    evt.preventDefault();
+    setAuthData({ ...authData, password: evt.target.value });
+  };
+
+  const handleSubmit = (evt: FormEvent<HTMLFormElement>): void => {
+    evt.preventDefault();
+    if (authData.login !== '' && authData.password !== '') {
+      dispatch(loginAction(authData));
+    }
+  };
 
   return (
     <div className="page page--gray page--login">
@@ -16,14 +42,35 @@ export default function LoginScreen(): JSX.Element {
         <div className="page__login-container container">
           <section className="login">
             <h1 className="login__title">Sign in</h1>
-            <form className="login__form form" action="#" method="post">
+            <form
+              className="login__form form"
+              action="#"
+              method="post"
+              onSubmit={handleSubmit}
+            >
               <div className="login__input-wrapper form__input-wrapper">
                 <label className="visually-hidden">E-mail</label>
-                <input className="login__input form__input" type="email" name="email" placeholder="Email" required />
+                <input
+                  className="login__input form__input"
+                  type="email"
+                  name="email"
+                  placeholder="Email"
+                  value={authData.login}
+                  onChange={handleLoginChange}
+                  required
+                />
               </div>
               <div className="login__input-wrapper form__input-wrapper">
                 <label className="visually-hidden">Password</label>
-                <input className="login__input form__input" type="password" name="password" placeholder="Password" required />
+                <input
+                  className="login__input form__input"
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                  value={authData.password}
+                  onChange={handlePasswordChange}
+                  required
+                />
               </div>
               <button className="login__submit form__submit button" type="submit">Sign in</button>
             </form>
