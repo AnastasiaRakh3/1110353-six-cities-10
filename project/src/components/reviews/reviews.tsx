a31ac8b5ecf3cc15b7a07ceaf93883bf61844158
Anastasiia Rakhmatova <rakhmatova-93@mail.ru>
2022-08-15 13:53:02 +0300

    Show reviews form for authorized user

--- project/src/components/reviews/reviews.tsx
+++ project/src/components/reviews/reviews.tsx
@@ -1,17 +1,24 @@
 import ReviewForm from '../review-form/review-form';
 import CommentsList from '../comments-list/comments-list';
 import { CommentType } from '../../types/comment';
+import { AuthorizationStatus } from '../../const';
+import { useAppSelector } from '../../hooks';
 
 type ReviewsProps = {
   reviews: CommentType[],
 };
 
 export default function Reviews({ reviews }: ReviewsProps): JSX.Element {
+
+  const authorizationStatus = useAppSelector((state) => state.authorizationStatus);
+
+  const isCommentsFormAvailable = authorizationStatus === AuthorizationStatus.Auth;
+
   return (
     <section className="property__reviews reviews">
       <h2 className="reviews__title">Reviews &middot; <span className="reviews__amount">{reviews.length}</span></h2>
       <CommentsList commentsList={reviews} />
-      <ReviewForm />
+      {isCommentsFormAvailable && <ReviewForm />}
     </section>
   );
 }
